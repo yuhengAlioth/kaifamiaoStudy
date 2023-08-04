4fb826b53b644870ffe913edaf008e32e9e664f4
七星玉蘅 <1532250426@qq.com>
2023-08-04 12:02:32 +0800

    练习题

---
+++ JAVA代码/练习题/practise2/src/main/java/LianXi/Lianxi0804.java
@@ -0,0 +1,7 @@
+package LianXi;
+
+public class Lianxi0804 {
+    public static void main(String[] args) {
+//        求指定两个数的最大公约数和最小公倍数
+    }
+}
---
+++ JAVA代码/练习题/practise2/src/main/java/LianXi/Lianxi0804_4.java
@@ -0,0 +1,18 @@
+package LianXi;
+
+public class Lianxi0804_4 {
+    public static void main(String[] args) {
+//        洪乞丐干10天，收入是多少？
+//        天朝有一个乞丐姓洪，去天桥要钱
+//                第一天要了1块钱
+//                第二天要了2块钱
+//                第三天要了4块钱
+//                第四天要了8块钱
+//
+        int money = 0;
+        for (int i = 1;i <= 10;i ++){
+            money = money +1;
+        }
+        System.out.println(money);
+    }
+}
