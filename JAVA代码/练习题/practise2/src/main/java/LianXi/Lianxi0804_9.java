2521e5d48aa6034db34816b04a500b605d9caab5
七星玉蘅 <1532250426@qq.com>
2023-08-07 22:20:19 +0800

    上传练习题答案

---
+++ JAVA代码/练习题/practise2/src/main/java/LianXi/Lianxi0804_9.java
@@ -0,0 +1,7 @@
+package LianXi;
+
+public class Lianxi0804_9 {
+    public static void main(String[] args) {
+        // 一个循环，打印输出一个由字符构成的正方形图案，其中边长由用户输入确定。要求字符按照以下顺序循环使用：**'#' , '@', '\*'**。
+    }
+}
