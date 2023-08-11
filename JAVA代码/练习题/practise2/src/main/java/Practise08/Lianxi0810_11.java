a32717d851a77c578a1f146b6c9b30bc27886cb1
七星玉蘅 <1532250426@qq.com>
2023-08-11 10:48:20 +0800

    上传代码

--- JAVA代码/练习题/practise2/src/main/java/Practise08/Lianxi0810_11.java
+++ JAVA代码/练习题/practise2/src/main/java/Practise08/Lianxi0810_11.java
@@ -11,13 +11,21 @@ public class Lianxi0810_11 {
     public static void main(String[] args) {
         // 编写一个Jva程序，使用循环输出所有的完美数。
         // 完美数是等于其所有因子之和的正整数，如6=1+2+3。
-        int num = 6;
-        // 判断是否是正整数
-        if (num > 0) {
-
-
-        }
+        perfectNumber(10000);
     }
 
+    public static void perfectNumber(int number) {
+        for (int i = 1; i < number; i++) {
+            int sum = 0;
+            for (int j = 1; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    sum += j;
+                }
+            }
+            if (sum == i) {
+                System.out.println(sum);
+            }
+        }
+    }
 }
 
--- JAVA代码/练习题/practise2/src/main/java/Practise08/Lianxi0810_9.java
+++ JAVA代码/练习题/practise2/src/main/java/Practise08/Lianxi0810_9.java
@@ -11,7 +11,19 @@ public class Lianxi0810_9 {
     public static void main(String[] args) {
         // 编写一个Java程序，使用循环计算π的近似值。
         // 可以使用Leibniz级数来近似π的值：π=4*（1-1/3+1/5-1/7+1/9-1/11+··.),通过增加级数项的数量来改进近似结果。
-
+        double res = isPI(100000000);
+        System.out.println("PI的近似值 = " + res);
+    }
+    public static double isPI(int num){
+        double res= 0;
+        for (int i = 1; i < num; i++) {
+            if (i % 2 == 0){
+                res += - 1.0/(2 * i -1);
+            }else {
+                res += 1.0/(2 * i -1);
+            }
+        }
+        return 4 * res;
     }
 
 }
