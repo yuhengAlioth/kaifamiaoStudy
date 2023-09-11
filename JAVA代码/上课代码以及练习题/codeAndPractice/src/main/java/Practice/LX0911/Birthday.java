fb4e2f8be8e7345c179bf70dc82ef28a30879e8c
七星玉蘅 <1532250426@qq.com>
2023-09-11 22:35:16 +0800

    上传代码

--- JAVA代码/上课代码以及练习题/codeAndPractice/src/main/java/Practice/LX0911/Birthday.java
+++ JAVA代码/上课代码以及练习题/codeAndPractice/src/main/java/Practice/LX0911/Birthday.java
@@ -1,5 +1,11 @@
 package Practice.LX0911;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 /**
  * @作者：玉蘅
  * @项目名称：codeAndPractice
@@ -11,7 +17,30 @@ package Practice.LX0911;
 public class Birthday {
     // 够根据日期提醒用户哪些人今天过生日？如何筛选显示出最近七天过生日的用户
     public static void main(String[] args) {
+        Map<String, LocalDate> map = new HashMap<>();
+        map.put("岳山", LocalDate.of(2019, 8, 4));
+        map.put("奥特曼", LocalDate.of(2038, 3, 5));
+        map.put("天海", LocalDate.of(2020, 2, 18));
+        map.put("无尘", LocalDate.of(2002, 5, 10));
 
+        LocalDate today = LocalDate.now();
+
+        Stream<Map.Entry<String, LocalDate>> entryStream = map.entrySet().stream().filter(new Predicate<Map.Entry<String, LocalDate>>() {
+            @Override
+            public boolean test(Map.Entry<String, LocalDate> stringLocalDateEntry) {
+                return stringLocalDateEntry.getValue().getMonth()==LocalDate.now().getMonth()
+                        && stringLocalDateEntry.getValue().getDayOfMonth()==LocalDate.now().getDayOfMonth();
+            }
+        });
+        Stream<Map.Entry<String, LocalDate>> entryStream1 = map.entrySet().stream().filter(new Predicate<Map.Entry<String, LocalDate>>() {
+            @Override
+            public boolean test(Map.Entry<String, LocalDate> stringLocalDateEntry) {
+                return stringLocalDateEntry.getValue().isBefore(LocalDate.now().minusDays(7))
+                        || stringLocalDateEntry.getValue().isAfter(LocalDate.now().plusDays(7));
+            }
+        });
+        entryStream.forEach(o -> System.out.println(o));
+        entryStream1.forEach(o -> System.out.println(o));
     }
 
 }
